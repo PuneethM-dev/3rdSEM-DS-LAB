3221e8607b98d04ad1d236ef9f16130483f7efff
PuneethM-dev <puneethm.bms@gmail.com>
2025-12-27 12:35:24 +0530

    Update Lab2_InfixToPostfix.c

--- Lab2_InfixToPostfix.c
+++ Lab2_InfixToPostfix.c
@@ -1,104 +1,97 @@
 #include <stdio.h>
 #include <ctype.h>
-#define MAX_SIZE 50
-char stack[MAX_SIZE];
-int top = -1;
-void push(char item)
-{
 
-    if(top < MAX_SIZE-1)
-    {
-        stack[++top] = item;
-    }
+#define MAX 50
+
+char stack[MAX];
+int top = -1;
+
+/* Stack operations */
+void push(char x)
+{
+    if (top == MAX - 1)
+        printf("Stack Overflow\n");
+    else
+        stack[++top] = x;
 }
 
 char pop()
 {
-
-    if(top >= 0)
-    {
+    if (top == -1)
+        return -1;
+    else
         return stack[top--];
-    }
-    return '\0';
 }
 
 char peek()
 {
-
-    if(!isEmpty())
-    {
-        return stack[top];
-    }
-    return '\0';
+    return stack[top];
 }
 
 int isEmpty()
 {
-    return top==-1;
+    return top == -1;
 }
 
-int precedence(char item)
+/* Precedence function */
+int precedence(char x)
 {
-    if(item == '^')
-        return 3;
-    if(item == '*' || item == '/' || item == '%')
-        return 2;
-    if(item == '+' || item == '-')
-        return 1;
+    if (x == '^') return 3;
+    if (x == '*' || x == '/' || x == '%') return 2;
+    if (x == '+' || x == '-') return 1;
     return 0;
 }
 
-void convertInfixToPostfix (char infix[] , char postfix[])
+/* Infix to Postfix conversion */
+void infixToPostfix(char infix[], char postfix[])
 {
-    int i=0 , j=0;
+    int i = 0, j = 0;
     char item;
-    while(infix[i] != '\0')
+
+    while (infix[i] != '\0')
     {
         item = infix[i];
-        if(isalnum(item))
+
+        if (isalnum(item))            // operand
         {
             postfix[j++] = item;
         }
-        else if(item == '(')
+        else if (item == '(')
         {
             push(item);
         }
-        else if(item == ')')
+        else if (item == ')')
         {
-            while(!isEmpty() && peek() != '(')
-            {
+            while (!isEmpty() && peek() != '(')
                 postfix[j++] = pop();
-            }
-            pop();
+            pop(); // remove '('
         }
-        else
+        else                          // operator
         {
-            while(!isEmpty() && precedence(peek())>=precedence(item))
-            {
+            while (!isEmpty() && precedence(peek()) >= precedence(item))
                 postfix[j++] = pop();
-            }
-
-                push(item);
-                //postfix[j++] = item;
+            push(item);
         }
-
         i++;
     }
-    while(!isEmpty())
-    {
+
+    while (!isEmpty())
         postfix[j++] = pop();
-    }
+
+    postfix[j] = '\0';
 }
 
+/* Main */
 int main()
 {
-    char infix[MAX_SIZE];
-    char postfix[MAX_SIZE];
-    top = -1;
-    printf("Enter the Infix expression = ");
-    scanf("%s" , infix);
-    printf("Infix Expression = %s\n" , infix);
-    convertInfixToPostfix(infix , postfix);
-    printf("Postfix Expression = %s\n" , postfix);
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter Infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
     return 0;
 }
